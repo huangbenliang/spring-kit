79eba54dcf8be7c162143ea3a927a8ed13cc7b59
huangbenliang <1372263592@qq.com>
2024-10-05 11:44:42 +0800

    spring boot 建立项目

---
+++ src/test/java/org/firsttime/springkit/SpringKitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.firsttime.springkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
