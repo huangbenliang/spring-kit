86349a63beeeca242729e571b127084f121c062c
huangbenliang <1372263592@qq.com>
2024-10-06 11:20:13 +0800

    health check

--- src/main/java/org/firsttime/springkit/SpringKitApplication.java
+++ src/main/java/org/firsttime/springkit/SpringKitApplication.java
@@ -2,12 +2,20 @@ package org.firsttime.springkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringKitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKitApplication.class, args);
     }
 
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
+    }
 }
